d340bb0803a0bb0cf94a3a76402865c0890f99b1
SeongChan Lee <foriequal@gmail.com>
2019-12-05 02:20:59 +0900

    Add 04

---
+++ src/bin/04-a.rs
@@ -0,0 +1,50 @@
+use std::error::Error;
+use std::io;
+use std::io::prelude::*;
+use std::ops::RangeInclusive;
+
+type BoxedError = Box<dyn Error + 'static>;
+type Result<T> = std::result::Result<T, BoxedError>;
+
+fn get_range<R: BufRead>(read: R) -> Result<RangeInclusive<u32>> {
+    let mut split = read.split(b'-');
+    let from = split.next().expect("No to")?;
+    let from = String::from_utf8(from)?.trim().parse::<u32>()?;
+    let to = split.next().expect("No to")?;
+    let to = String::from_utf8(to)?.trim().parse::<u32>()?;
+    Ok(from..=to)
+}
+
+fn is_password(x: u32) -> bool {
+    let mut buf = [0u8; 6];
+    if write!(&mut buf[..], "{}", x).is_err() || buf.contains(&0) {
+        return false;
+    }
+    let mut double = false;
+    for (a, b) in buf[0..buf.len() - 1].iter().zip(&buf[1..]) {
+        if a > b {
+            return false;
+        }
+        if a == b {
+            double = true;
+        }
+    }
+    double
+}
+
+fn run<R: BufRead>(read: R) -> Result<(usize)> {
+    Ok(get_range(read)?.filter(|x| is_password(*x)).count())
+}
+
+fn main() -> Result<()> {
+    let stdin = io::stdin();
+    let result = run(stdin.lock())?;
+    println!("result: {}", result);
+    Ok(())
+}
+
+#[test]
+fn test() {
+    let input = include_bytes!("../../input/04");
+    assert_eq!(run(&input[..]).ok(), Some(544));
+}
